0bb5d3ef534ab3ef337479b7ddfddb0dd0f4f365
ahmedayman305 <ahmedaymankamel78@gmail.com>
2024-08-08 00:43:20 +0300

    make a navbar & compelete Home page

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+    title: "promptopia",
+};
+
+const layout = ({ children }) => {
+    return (
+        <html lang="en">
+            <head>
+                <link rel="icon" href="/assets/images/logo.svg" />
+            </head>
+            <body>
+                <Provider>
+                    <div className="main">
+                        <div className="gradient"></div>
+                    </div>
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    );
+};
+
+export default layout;
