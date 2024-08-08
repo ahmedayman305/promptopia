a63224442687a3fde7f001d3d114158969bd874c
ahmedayman305 <ahmedaymankamel78@gmail.com>
2024-08-08 20:27:03 +0300

    finish the project

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,51 +1,51 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 import Form from "@components/Form";
-import axios from "axios";
 
 const CreatePrompt = () => {
-    const { data: session } = useSession();
-    const router = useRouter();
-    const [submitting, setSubmitting] = useState(false);
-    const [post, setPost] = useState({
-        prompt: " ",
-        tag: "",
-    });
+  const router = useRouter();
+  const { data: session } = useSession();
 
-    const createPrompt = async (e) => {
-        e.preventDefault();
-        setSubmitting(true);
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
-        try {
-            const data = {
-                prompt: post.prompt,
-                userId: session?.user.id,
-                tag: post.tag,
-            };
-            const res = await axios.post("/api/prompt/new", data);
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
 
-            if (res.status === 201) {
-                router.push("/");
-            }
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setSubmitting(false);
-        }
-    };
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
 
-    return (
-        <Form
-            type="Create"
-            post={post}
-            setPost={setPost}
-            submitting={submitting}
-            handleSubmit={createPrompt}
-        />
-    );
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,28 @@
 import "@styles/globals.css";
+
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "promptopia",
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
 };
 
-const layout = ({ children }) => {
-    return (
-        <html lang="en">
-            <head>
-                <link rel="icon" href="/assets/images/logo.svg" />
-            </head>
-            <body>
-                <Provider>
-                    <div className="main">
-                        <div className="gradient"></div>
-                    </div>
-                    <main className="app">
-                        <Nav />
-                        {children}
-                    </main>
-                </Provider>
-            </body>
-        </html>
-    );
-};
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-export default layout;
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
